0642d95b662af5617cc1dbd45c579af875cc1622
Stheven <sthevenalvess@gmail.com>
2024-05-23 10:28:12 -0300

    feat: create LoginResponse to accessToken

---
+++ src/main/java/com/sthev/spotify/client/LoginResponse.java
@@ -0,0 +1,24 @@
+package com.sthev.spotify.client;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class LoginResponse {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    public LoginResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public LoginResponse() {
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
